b8f8587d94b4ac1fb4f38ee159e481f80cd357e0
Dan Ranges <danranges@gmail.com>
2022-10-03 22:32:03 +0200

    Add and test firestore write method

--- src/data/firebase.tsx
+++ src/data/firebase.tsx
@@ -1,12 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore/lite"
-import { getAnalytics } from "firebase/analytics"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
+import { collection, addDoc } from "firebase/firestore"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA9D2NCPu34ewa419S-o-ht8wnDOJWXHss",
   authDomain: "ts-todo-a09b7.firebaseapp.com",
@@ -19,7 +15,17 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
-const analytics = getAnalytics(app)
+export const db = getFirestore(app)
 
-export { db }
+const testAdd = async () => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815,
+    })
+    console.log("Document written with ID: ", docRef.id)
+  } catch (e) {
+    console.error("Error adding document: ", e)
+  }
+}
